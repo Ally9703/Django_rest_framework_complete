f7f1b8ad63c1df6e0334104de5e765ad15282308
Ally9703 <allytshindayi@gmail.com>
2024-03-22 22:50:21 +0100

    dans cette partie j'ai utiliser le model Serialiser

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -1,38 +1,46 @@
 from rest_framework import serializers
 from watchlist_app.models import Movie
-from watchmate.settings import DATABASES
+
 
 def name_length(value):
         if len(value) < 2:
             raise serializers.ValidationError('Name is too short')
         
-class MovieSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(validators=[name_length])
-    description = serializers.CharField()
-    active = serializers.BooleanField()
+class MovieSerializer(serializers.ModelSerializer):
     
-    def create(self, validated_data):
-        return Movie.objects.create(**validated_data)
-    
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
-        instance.description = validated_data.get('description', instance.description)
-        instance.active = validated_data.get('active', instance.active)
-        instance.save()
-        return instance
-    
-    
-       
+    class Meta:
+        model = Movie
+        fields = "__all__"
         
     def validate(self, data):
         if data['name'] == data['description']:
             raise serializers.ValidationError('Title and description should be different')
         else:
             return data
+
+    def validate_name(self, value):
+        if len(value) < 2:
+            raise serializers.ValidationError('Name is too short')
+        else:
+            return value
     
-    # def validate_name(self, value):
-    #     if len(value) < 2:
-    #         raise serializers.ValidationError('Name is too short')
-    #     else:
-    #         return value
+    
+    # id = serializers.IntegerField(read_only=True)
+    # name = serializers.CharField(validators=[name_length])
+    # description = serializers.CharField()
+    # active = serializers.BooleanField()
+    
+    # def create(self, validated_data):
+    #     return Movie.objects.create(**validated_data)
+    
+    # def update(self, instance, validated_data):
+    #     instance.name = validated_data.get('name', instance.name)
+    #     instance.description = validated_data.get('description', instance.description)
+    #     instance.active = validated_data.get('active', instance.active)
+    #     instance.save()
+    #     return instance
+    
+    
+       
+        
+    

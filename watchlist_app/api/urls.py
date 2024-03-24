dd6ee5fd99ce7874f8c4104edc09ef90cae77246
Ally9703 <allytshindayi@gmail.com>
2024-03-24 20:40:26 +0100

    updating Models

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from watchlist_app.api.views import MovieListAV, MovieDetailAV
+from watchlist_app.api.views import WatchDetailAV,WatchListAV,  StreamPlatformAv
 
 urlpatterns = [
-    path('list/', MovieListAV.as_view(), name='movie_list'),
-    path('<int:pk>/', MovieDetailAV.as_view(), name='movie_detail'),
+    path('list/', WatchListAV.as_view(), name='movie_list'),
+    path('<int:pk>/', WatchDetailAV.as_view(), name='movie_detail'),
+    path('stream/', StreamPlatformAv.as_view(), name='stream_platform'),
 ]

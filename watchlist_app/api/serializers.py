6eeb8cb5f09206efe493f8cea9ca8c91d2a475b7
Ally9703 <allytshindayi@gmail.com>
2024-03-24 19:38:29 +0100

    custom serializer Fields

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -7,11 +7,14 @@ def name_length(value):
             raise serializers.ValidationError('Name is too short')
         
 class MovieSerializer(serializers.ModelSerializer):
-    
+    len_name = serializers.SerializerMethodField()
     class Meta:
         model = Movie
         fields = "__all__"
-        
+        #fields = ['id','name', 'description', ]
+        # exclude = ['name']
+    def get_len_name(self, object):
+        return len(object.name)
     def validate(self, data):
         if data['name'] == data['description']:
             raise serializers.ValidationError('Title and description should be different')

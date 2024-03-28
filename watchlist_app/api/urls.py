3fa8120193a5f6e2f6d5a4f095733f922971222e
Ally9703 <allytshindayi@gmail.com>
2024-03-28 09:42:17 +0100

    url structure

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from watchlist_app.api.views import WatchListAV,  StreamPlatformAV, StreamPlatformDetailAV
+from watchlist_app.api.views import ReviewList,ReviewDetail, WatchListAV,  StreamPlatformAV, StreamPlatformDetailAV
 
 urlpatterns = [
+    
     path('list/', WatchListAV.as_view(), name='movie_list'),
-    # path('<int:pk>/', WatchDetailAV.as_view(), name='movie_detail'),
+    # path('<int:pk>/', WatchDetailAV.as_view(), name='movie-detail'),
     path('stream/', StreamPlatformAV.as_view(), name='stream_platform'),
     path('stream/<int:pk>', StreamPlatformDetailAV.as_view(), name='streamplatform-detail'),
+    
+    path('review/', ReviewList.as_view(), name='review-list'),
+    path('review/<int:pk>', ReviewDetail.as_view(), name='review-detail'),
+    
 ]
--- watchlist_app/models.py
+++ watchlist_app/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.core.validators import MinValueValidator, MaxValueValidator
 # Create your models here.
 class StreamPlatform(models.Model):
     name    = models.CharField(max_length=30)
@@ -18,3 +18,14 @@ class WatchList(models.Model):
     
     def __str__(self):
         return self.title
+    
+class Review(models.Model):
+    rating      = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    description = models.CharField(max_length=100)
+    watchList   = models.ForeignKey(WatchList, on_delete=models.CASCADE, related_name="reviews")
+    active      = models.BooleanField(default=True)
+    created     = models.DateTimeField(auto_now_add=True)
+    updated     = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return str(self.rating) + " | "  + self.watchList.title
